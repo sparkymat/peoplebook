607cc1ff1f9f500c5fd1e5e3449ebdc818b9fbf5
Ajith Hussain <ajith.hussain@fastmail.com>
2022-07-10 21:11:29 +1000

    WIP

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 class Person < ApplicationRecord
-  has_one :child_relation, foreign_key: :child_id
+  has_one :regular_child_relation, -> { merge(ChildRelation.regular) }, foreign_key: :child_id, class_name: "ChildRelation"
+  has_one :biological_child_relation, -> { merge(ChildRelation.biological) }, foreign_key: :child_id, class_name: "ChildRelation"
+has_one :adopted_child_relation, -> { merge(ChildRelation.adopted) }, foreign_key: :child_id, class_name: "ChildRelation"
 
   enum gender: {
     male:   "male",
@@ -8,20 +10,45 @@ class Person < ApplicationRecord
     other:  "other",
   }
 
-  def find_relations
+  def self.sibling_relation(gender)
+    {
+      "female" => :sister,
+      "male"   => :brother,
+    }[gender]
+  end
+
+  def self.parent_relation(gender)
+    {
+      "female" => :mother,
+      "male"   => :father,
+    }[gender]
+  end
+
+  def regular_child_relations
+  end
+
+  def relations
     relations = []
-    if child_relation.present?
-      partnership = child_relation.partnership
 
-      # parents
-      parents = Person.where(id: partnership.slice(:person1_id, :person2_id).values)
-      relations += parents.map{|p| [{"female"=>:mother,"male"=>:father}[p.gender],p,["ChildRelation##{child_relation.id}","Partnership##{partnership.id}"]]}
+    # As a regular child
+    if regular_child_relation&.partnership.present?
 
-      # siblings
-      sibling_relations = ChildRelation.where(partnership:).where.not(child_id: id).includes(:child)
-      relations += sibling_relations.map{|s| [{"female"=>:sister, "male"=>:brother}[s.child.gender], s.child,["ChildRelation##{child_relation.id}","Partnership##{partnership.id}","ChildRelation##{s.id}"]]}
+      parents = regular_child_relation.partnership.slice(:person1, :person2).values
+
+      relations += parents.map do |p|
+        {
+          relation: :parent,
+          person: p,
+          links: [
+            "ChildRelation##{regular_child_relation.id}",
+            "Partnership##{regular_child_relation.partnership_id}",
+          ]
+        }
+      end
     end
 
+    # As a parent
+    
     relations
   end
 end
