5e4f0d114865dfcffc353d8e74ebc0d9d4947cdd
Ajith Hussain <ajith.hussain@fastmail.com>
2022-07-13 22:44:06 +1000

    Added more data

--- db/fixtures/03_partnerships.rb
+++ db/fixtures/03_partnerships.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Partnership.seed(:id,
-  {id: 1, person1_id: 2, person2_id: 3, started_at: DateTime.parse("1979-01-01")},
-  {id: 2, person1_id: 4, person2_id: 5, started_at: DateTime.parse("1969-01-01")},
+  {id: 1, person1_id:  2, person2_id: 3, started_at: DateTime.parse("1979-01-01")},
+  {id: 2, person1_id:  4, person2_id: 5, started_at: DateTime.parse("1969-01-01")},
+  {id: 3, person1_id:  1, person2_id: 12, started_at: DateTime.parse("2002-01-01")},
+  {id: 4, person1_id: 11, person2_id: 16, started_at: DateTime.parse("2003-01-01")},
 )
